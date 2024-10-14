ccdab2d535e54966754f0db994040b5b6d6a7c70
ChristianSorace9226 <soracechristian0@gmail.com>
2024-10-14 16:19:12 +0200

    FIX/ Fixato il servizio di prenotazione. FEAT/ Add AppValue FEAT/ Add ExternalCall

---
+++ src/main/java/it/access/prenotazione/config/AppValue.java
@@ -0,0 +1,14 @@
+package it.access.prenotazione.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public class AppValue {
+
+    @Value("${external.service.url}")
+    private String externalServiceUrl;
+
+}
